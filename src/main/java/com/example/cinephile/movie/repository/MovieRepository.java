c3856f5907eae5e8a378b2f11117509083846ad4
Abobakr Osman <mabobakr79@yahoo.com>
2025-09-15 22:05:06 +0300

    feat(movie): add method to get all and search for movies

--- src/main/java/com/example/cinephile/movie/repository/MovieRepository.java
+++ src/main/java/com/example/cinephile/movie/repository/MovieRepository.java
@@ -1,9 +1,11 @@
 package com.example.cinephile.movie.repository;
 
+import com.example.cinephile.movie.dto.MovieCard;
 import com.example.cinephile.movie.entity.Movie;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -11,4 +13,16 @@ import java.util.UUID;
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, UUID> {
     Page<Movie> findByTitleContainingIgnoreCase(String title, Pageable pageable);
+
+    @Query("""
+            SELECT new com.example.cinephile.movie.dto.MovieCard(
+            m.id, m.title, m.posterUrl, m.year, m.rated, m.rating
+            )
+            FROM Movie m
+            WHERE (:title IS NULL OR LOWER(m.title) LIKE LOWER(CONCAT('%', :title, '%')))
+            AND (:genre IS NULL OR LOWER(m.genre) LIKE LOWER(CONCAT('%', :genre, '%')))
+            AND (:rated IS NULL OR LOWER(m.rated) = LOWER(:rated))
+            AND (:rating IS NULL OR m.rating >= :rating)
+            """)
+    Page<MovieCard> findAllMovies(String title, String genre, String rated, Double rating, Pageable pageable);
 }
