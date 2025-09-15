2cb39efea737eddbb5c222097af74412b6b43be1
Abobakr Osman <mabobakr79@yahoo.com>
2025-09-15 22:08:27 +0300

    feat(users): add method to find users with search and pagination

--- src/main/java/com/example/cinephile/user/repository/UserRepository.java
+++ src/main/java/com/example/cinephile/user/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.example.cinephile.user.repository;
 
+import com.example.cinephile.user.entity.Role;
 import com.example.cinephile.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,4 +17,13 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByEmail(String email);
     Optional<User> findByVerificationToken(String verificationToken);
     Optional<User> findByResetPasswordToken(String token);
+
+    @Query("""
+        SELECT u FROM User u
+        WHERE (:name IS NULL OR LOWER(u.name) LIKE LOWER(CONCAT('%', :name, '%')))
+        AND (:email IS NULL OR LOWER(u.email) LIKE LOWER(CONCAT('%', :email, '%')))
+        AND (:role IS NULL OR u.role = :role)
+        AND (:enabled IS NULL OR u.enabled = :enabled)
+        """)
+    Page<User> findAllUsers(String name, String email, Role role, Boolean enabled, Pageable pageable);
 }
