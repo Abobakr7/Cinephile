008e1ec8d41422171139449e9a725edc97f80b16
Abobakr Osman <mabobakr79@yahoo.com>
2025-09-19 00:15:55 +0300

    feat(cinema): add find cinemas by movie id & exists by id & manager id

--- src/main/java/com/example/cinephile/cinema/repository/CinemaRepository.java
+++ src/main/java/com/example/cinephile/cinema/repository/CinemaRepository.java
@@ -4,6 +4,7 @@ import com.example.cinephile.cinema.entity.Cinema;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +15,15 @@ public interface CinemaRepository extends JpaRepository<Cinema, UUID> {
     Page<Cinema> findByNameContainingIgnoreCase(String search, Pageable pageable);
 
     List<Cinema> findByManagerId(UUID managerId);
+
+    @Query("""
+            SELECT DISTINCT c FROM Cinema c
+            JOIN Showtime st ON st.cinema = c
+            WHERE st.movie.id = :movieId
+                AND st.isActive = true
+                AND st.startTime > CURRENT_TIMESTAMP
+            """)
+    List<Cinema> findCinemasByMovieId(UUID movieId);
+
+    boolean existsByIdAndManagerId(UUID cinemaId, UUID managerId);
 }
